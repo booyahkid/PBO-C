baa74186308c47750e643cf5e3e211f124ad9ec3
booyahkid <100134142+booyahkid@users.noreply.github.com>
2023-05-04 12:45:03 +0700

    Update Pegawai.java

--- Praktikum_7/Pegawai/Pegawai.java
+++ Praktikum_7/Pegawai/Pegawai.java
@@ -1,3 +1,9 @@
+/**
+ * File      : Pegawai.java		04/05/23
+ * Penulis   : Fikri Baswara Andanawarih
+ * Deskripsi : representasi dari pegawai
+ */
+
 public class Pegawai{
     private String nama;
     private int gajiPokok = 5000000;
