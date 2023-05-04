e41f34cb85ad50725cc15c051fe067fde4399fa5
booyahkid <100134142+booyahkid@users.noreply.github.com>
2023-05-04 12:44:25 +0700

    Update Payroll.java

--- Praktikum_7/Pegawai/Payroll.java
+++ Praktikum_7/Pegawai/Payroll.java
@@ -1,3 +1,9 @@
+/**
+* File      : Payroll.java		04/05/23
+* Penulis   : Fikri Baswara Andanawarih
+* Deskripsi : representasi dari payroll
+*/
+
 public class Payroll{
     void cetakGaji(Pegawai p){
         p.tampilData();
