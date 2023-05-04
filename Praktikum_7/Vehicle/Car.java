96140fb87f3d3c4d913d6b9bf6af39310356117d
booyahkid <100134142+booyahkid@users.noreply.github.com>
2023-05-04 12:41:50 +0700

    Update Car.java

--- Praktikum_7/Vehicle/Car.java
+++ Praktikum_7/Vehicle/Car.java
@@ -1,3 +1,9 @@
+/**
+* File      : Car.java		04/05/23
+* Penulis   : Fikri Baswara Andanawarih
+* Deskripsi : representasi dari car
+*/
+
 public class Car extends Vehicle{
     void calRent(int jarak, float harga){
         float fare = jarak * harga;
