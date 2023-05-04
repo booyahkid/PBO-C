8648a6ccb8b6b604aabe438135a12d3d4b2a6d62
booyahkid <100134142+booyahkid@users.noreply.github.com>
2023-05-04 12:40:31 +0700

    Update Vehicle.java

--- Praktikum_7/Vehicle/Vehicle.java
+++ Praktikum_7/Vehicle/Vehicle.java
@@ -1,3 +1,9 @@
+/**
+ * File      : Vehicle.java		15/03/23
+ * Penulis   : Fikri Baswara Andanawarih
+ * Deskripsi : Vehicle
+ */
+
 public class Vehicle{
     void calRent(int distance, float price) {
         float fare = distance * price;
